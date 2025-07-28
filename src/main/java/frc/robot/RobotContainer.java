2586ea4fcdf85fe5cb13c337b3f5e94762a0a8e7
Dylan Lu <dylannlu@icloud.com>
2025-07-28 10:11:50 -0700

    Test

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -1,18 +1,22 @@
 package frc.robot;
 
-import frc.robot.Constants.OperatorConstants;
-import frc.robot.commands.AutoMove;
-import frc.robot.subsystems.Motor;
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.commands.AutoMove;
+import frc.robot.subsystems.Motor;
+import frc.robot.Constants.OperatorConstants;
+import edu.wpi.first.wpilibj.XboxController;
 
 public class RobotContainer {
-  private final Joystick buttonJoystick = new Joystick();
+  private final Joystick buttonJoystick = new Joystick(OperatorConstants.kDriverControllerPort);
   private final Motor m_Motor = new Motor();
+  private final JoystickButton activateAutoTurn = new JoystickButton(buttonJoystick);
 
-  public RobotContainer() {
-  
+  public RobotContainer(new AutoMove, () -> Motor.getAutonomousCommand ) {
+
     configureBindings();
+    
+    
   }
 
   private void configureBindings() {
